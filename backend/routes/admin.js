dcb24da6cee93fdc1f88efc5b688cc65562b6797
unknown <rohitgiri2472@gmail.com>
2026-06-06 02:44:24 +0530

    fixed model filename

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const Admin = require("../models/Admin");
-const User = require("../models/user");
+const User = require("../models/User");
 const Student = require("../models/student");
 
 
