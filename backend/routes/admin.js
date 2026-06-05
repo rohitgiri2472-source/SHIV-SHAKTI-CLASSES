dff71aa385606598b53efb2a85c9dfcdcd30a9ba
unknown <rohitgiri2472@gmail.com>
2026-06-06 03:20:46 +0530

    fixed Admin and User imports

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const Admin = require("../models/admin");
+const Admin = require("../models/Admin");
 const User = require("../models/User");
 const Student = require("../models/student");
 
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const User = require("../models/User");   // ✅ correct path
+const User = require("../models/User"); 
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
