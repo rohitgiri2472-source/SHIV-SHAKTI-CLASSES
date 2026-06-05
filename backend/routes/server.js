7164b24e7c7ebdbc9b27cb381a28f61fe4153ed0
unknown <rohitgiri2472@gmail.com>
2026-06-06 02:09:49 +0530

    installed bcryptjs

--- backend/routes/server.js
+++ backend/routes/server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cors());
 
 // Routes
-const authRoutes = require("./auth");
+const authRoutes = require('./routes/auth');
 const authMiddleware = require("../middleware/authMiddleware");
 const enrollmentRoutes = require("./enrollment");
 const adminRoutes = require("./admin");
